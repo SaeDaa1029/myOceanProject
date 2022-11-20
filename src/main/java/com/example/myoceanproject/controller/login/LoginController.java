fe27474c5d9b1ca208c42913c05d5544c37265a9
leehoyoung <whei1234@naver.com>
2022-11-20 19:35:01 +0900

    이호영 2211201932 작업사항

--- src/main/java/com/example/myoceanproject/controller/login/LoginController.java
+++ src/main/java/com/example/myoceanproject/controller/login/LoginController.java
@@ -13,4 +13,28 @@ public class LoginController {
     public String login(){
         return "app/login/login";
     }
+
+    // 로그인 페이지
+    @GetMapping("/findPw")
+    public String findPw(){
+        return "app/login/findPw";
+    }
+
+    // 로그인 페이지
+    @GetMapping("/findPwComplete")
+    public String findPwComplete(){
+        return "app/login/findPwComplete";
+    }
+
+    // 로그인 페이지
+    @GetMapping("/changePassword")
+    public String changePassword(){
+        return "app/login/changePassword";
+    }
+
+    // 로그인 페이지
+    @GetMapping("/changePwComplete")
+    public String changePwComplete(){
+        return "app/login/changePwComplete";
+    }
 }
